8995a06f2546f092dd11e531700f9797d20bf9bd
moinho <leandro.salustriano@hotmail.com>
2024-12-18 12:10:56 -0300

    feat(ethers-rps): agora conn impl arc

--- src/repositories/ethers_repository.rs
+++ src/repositories/ethers_repository.rs
@@ -1,7 +1,9 @@
 use ethers::providers::{Provider, Ws};
 use std::collections::HashMap;
+use std::sync::Arc;
+
 pub struct EthersRepository {
-    connections: HashMap<i32, Provider<Ws>>
+    connections: HashMap<i32, Arc<Provider<Ws>>>
 }
 
 impl EthersRepository {
@@ -13,11 +15,12 @@ impl EthersRepository {
     }
 
     pub fn apply_connection(&mut self, user_id:i32,provider:Provider<Ws>) {
+        let provider = Arc::new(provider);
         self.connections.insert(user_id,provider);
     }
 
-    pub fn get_connection(&self, user_id:i32) -> Option<&Provider<Ws>> {
-        self.connections.get(&user_id)
+    pub fn get_connection(&self, user_id:i32) -> Option<Arc<Provider<Ws>>> {
+        self.connections.get(&user_id).cloned()
     }
 
 }
--- src/services/ethers/get_logs_service.rs
+++ src/services/ethers/get_logs_service.rs
@@ -1,16 +1,10 @@
 use crate::repositories::elastic_repository::ElasticRepository;
 use crate::repositories::ethers_repository::EthersRepository;
-use elasticsearch::http::request::JsonBody;
-use ethers::core::k256::elliptic_curve::bigint::Encoding;
-use ethers::core::k256::U256;
 use ethers::middleware::Middleware;
 use ethers::prelude::{BlockNumber, Transaction};
 use ethers::types::{Block, BlockId};
-use ethers::utils::hex::hex;
-use ethers::utils::keccak256;
 use futures::future::join_all;
 use serde_json::json;
-use std::future::Future;
 use std::sync::{Arc, RwLock};
 use tokio::task;
 
@@ -59,14 +53,13 @@ impl GetLogsService {
 
     pub async fn exec(&self, user_id: i32, from_block: u64, to_block: u64) {
         let lock_provider = self.repository.read().unwrap();
-        let provider = lock_provider.get_connection(user_id).expect("ERR ");
-
         let mut handles = Vec::new();
 
         for block_number in from_block..=to_block {
-            let provider = provider.clone();
+            let provider = lock_provider.get_connection(user_id).expect("ERR ");
 
             let handle = task::spawn(async move {
+
                 let block_id = BlockId::Number(BlockNumber::Number(block_number.into()));
                 let logs = provider.get_block_with_txs(block_id).await;
                 println!("Block {block_number} read!");
