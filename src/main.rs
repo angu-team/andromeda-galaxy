b108fe6871c867374068da60bd0f97b29e690d44
moinho <leandro.salustriano@hotmail.com>
2024-12-22 23:47:18 -0300

    feat(listen-deploy-srv): ouvir deploys erc20

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 mod controllers;
+pub mod http_client;
 mod repositories;
 mod services;
-pub mod http_client;
 
 use crate::controllers::ethers_controller::EthersController;
+use crate::repositories::elastic_repository::ElasticRepository;
 use crate::repositories::ethers_repository::EthersRepository;
 use crate::repositories::redis_repository::RedisRepository;
-use crate::repositories::elastic_repository::ElasticRepository;
 use crate::services::ethers::apply_rpc_service::ApplyRpcService;
 
 use dotenv::dotenv;
 use std::env;
 use std::sync::{Arc, RwLock};
 
+use crate::services::elastic::get_erc20_contracts_service::{GetErc20ContractsService};
 use crate::services::ethers::get_logs_service::GetLogsService;
+use crate::services::ethers::listen_deploy_erc20_contracts_service::ListenDeployErc20ContractsService;
 use actix_web::{middleware::Logger, web, App, HttpServer};
-use redis::{Commands, FromRedisValue};
 use http_client::HttpClient;
+use redis::{Commands, FromRedisValue};
+use crate::controllers::elastic_controller::ElasticController;
 
 #[actix_web::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     dotenv().ok();
-    let http_client = HttpClient::new();
 
     HttpServer::new(move || {
         let mut app = App::new().wrap(Logger::default());
@@ -39,18 +41,39 @@ async fn main() {
         let redis_repository = Arc::new(RedisRepository::connect());
         let ethers_repository = Arc::new(RwLock::new(EthersRepository::new()));
 
+        let get_erc20_contracts = Arc::new(GetErc20ContractsService::new(
+            elastic_repository.clone()
+        ));
+
         let apply_rpc_service = Arc::new(ApplyRpcService::new(
             ethers_repository.clone(),
             redis_repository.clone(),
         ));
 
-        let get_logs_service = Arc::new(GetLogsService::new(ethers_repository.clone(),elastic_repository));
+        let get_logs_service = Arc::new(GetLogsService::new(
+            ethers_repository.clone(),
+            elastic_repository.clone(),
+        ));
+
+        let listen_deploy_erc20_contracts_service = Arc::new(
+            ListenDeployErc20ContractsService::new(ethers_repository.clone()),
+        );
 
         app = app.app_data(web::Data::new(apply_rpc_service.clone()));
+        app = app.app_data(web::Data::new(get_erc20_contracts.clone()));
         app = app.app_data(web::Data::new(get_logs_service.clone()));
+        app = app.app_data(web::Data::new(
+            listen_deploy_erc20_contracts_service.clone(),
+        ));
 
         let ethers_controller = EthersController::new();
+        let elastic_controller = ElasticController::new();
+
         for (endpoint, route) in ethers_controller.routes() {
+            app = app.route(&endpoint, route);
+        }
+
+        for (endpoint, route) in elastic_controller.routes() {
             app = app.route(&endpoint, route);
         }
 
